8df217e4d86ee7f3585a94801fdd5672ae8d2213
Dong-Yun Park <ehddbs7458@gmail.com>
2023-04-03 20:07:40 +0900

    Init Spring Boot framework

---
+++ src/main/java/com/richasha/musicpostbackend/MusicPostBackendApplication.java
@@ -0,0 +1,13 @@
+package com.richasha.musicpostbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicPostBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicPostBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/richasha/musicpostbackend/MusicPostBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richasha.musicpostbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicPostBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
