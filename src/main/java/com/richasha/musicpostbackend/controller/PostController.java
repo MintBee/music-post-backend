0fa68c505eb0de5374abf76d83f61e458c821a3e
Dong-Yun Park <ehddbs7458@gmail.com>
2023-05-18 19:05:15 +0900

    Doomed Commit

--- src/main/java/com/richasha/musicpostbackend/controller/PostController.java
+++ src/main/java/com/richasha/musicpostbackend/controller/PostController.java
@@ -1,11 +1,14 @@
 package com.richasha.musicpostbackend.controller;
 
+import com.richasha.musicpostbackend.dto.PointDto;
 import com.richasha.musicpostbackend.dto.PostDto;
 import com.richasha.musicpostbackend.entity.PostEntity;
+import com.richasha.musicpostbackend.entity.util.GeometryUtil;
 import com.richasha.musicpostbackend.mapper.entitydto.PostMapper;
 import com.richasha.musicpostbackend.repo.PostRepository;
 import com.richasha.musicpostbackend.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.locationtech.jts.geom.Point;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -22,9 +25,13 @@ public class PostController {
     @GetMapping
     public List<PostDto> getPosts(
             @RequestParam(value = "page", defaultValue = "0") int page,
-            @RequestParam(value = "size", defaultValue = "10") int size
+            @RequestParam(value = "size", defaultValue = "10") int size,
+            @RequestParam(value = "currentLongitude") double longitude,
+            @RequestParam(value = "currentLatitude") double latitude,
+            @RequestParam(value = "radius") int radius
     ) throws Exception {
-        return postService.getPaginatedPosts(page, size)
+        Point currentPoint = GeometryUtil.parseLocation(longitude, latitude);
+        return postService.getPaginatedPostsByDistance(page, size, currentPoint, radius)
                 .stream().map(postMapper::toDto).toList();
     }
 
