2d65a470fb6130cce08f596444fd49bce043b043
Dong-Yun Park <ehddbs7458@gmail.com>
2023-05-31 14:56:43 +0900

    Add PinDto

--- src/main/java/com/richasha/musicpostbackend/dto/PinDto.java
+++ src/main/java/com/richasha/musicpostbackend/dto/PinDto.java
@@ -1,8 +1,18 @@
 package com.richasha.musicpostbackend.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * A DTO for the {@link com.richasha.musicpostbackend.entity.PinEntity} entity
  */
+@Data
+@NoArgsConstructor
 public class PinDto {
-
+    private Long id;
+    private UserDto originalPoster;
+    private MusicDto music;
+    private String address;
+    private String locationName;
+    private PointDto coordinate;
 }
